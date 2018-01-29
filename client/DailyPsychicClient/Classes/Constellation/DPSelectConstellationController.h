136943fd58338ae81292937ed3b53d61f37a64a8
lishaoyan <619112452@qq.com>
2018-01-29 22:40:30 +0800

    create constellation file

---
+++ client/DailyPsychicClient/Classes/Constellation/DPSelectConstellationController.h
@@ -0,0 +1,13 @@
+//
+//  DPSelectConstellationController.h
+//  DailyPsychicClient
+//
+//  Created by 李少艳 on 2018/1/29.
+//  Copyright © 2018年 h. All rights reserved.
+//
+
+#import "BUCustomViewController.h"
+
+@interface DPSelectConstellationController : BUCustomViewController
+
+@end
---
+++ client/DailyPsychicClient/Classes/Constellation/DPSelectConstellationView.m
@@ -0,0 +1,21 @@
+//
+//  DPSelectConstellationView.m
+//  DailyPsychicClient
+//
+//  Created by 李少艳 on 2018/1/29.
+//  Copyright © 2018年 h. All rights reserved.
+//
+
+#import "DPSelectConstellationView.h"
+
+@implementation DPSelectConstellationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
