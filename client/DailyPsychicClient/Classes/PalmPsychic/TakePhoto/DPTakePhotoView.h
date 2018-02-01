436797dab55614226ad6f77702579a782cc0e883
MrHalloween <445218870@qq.com>
2018-02-01 15:45:47 +0800

    首相分析拍照

---
+++ client/DailyPsychicClient/Classes/PalmPsychic/TakePhoto/DPTakePhotoView.h
@@ -0,0 +1,23 @@
+//
+//  DPTakePhotoView.h
+//  DailyPsychicClient
+//
+//  Created by zhanghe on 2018/2/1.
+//  Copyright © 2018年 h. All rights reserved.
+//
+
+#import "AFBaseTableView.h"
+
+@protocol DPTakePhotoViewDelegate <NSObject>
+
+@optional
+- (void)TakePhoto;
+
+@end
+
+@interface DPTakePhotoView : AFBaseTableView
+
+@property (nonatomic,weak) id<DPTakePhotoViewDelegate>takePhotoDelegate;
+
+
+@end
