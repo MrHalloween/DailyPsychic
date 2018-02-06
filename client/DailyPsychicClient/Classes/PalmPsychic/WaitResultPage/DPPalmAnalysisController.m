e33f091acceb213365ea2da870868f842d0554e4
kattrina <619112452@qq.com>
2018-02-06 09:47:56 +0800

    内购代码整理

--- client/DailyPsychicClient/Classes/PalmPsychic/WaitResultPage/DPPalmAnalysisController.m
+++ client/DailyPsychicClient/Classes/PalmPsychic/WaitResultPage/DPPalmAnalysisController.m
@@ -23,157 +23,77 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
-//    m_pPalmAnalysisView = [[DPPalmAnalysisView alloc]initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height)];
-//    m_pPalmAnalysisView.proDelegate = self;
-//    [self.view addSubview:m_pPalmAnalysisView];
-    
-    [self settingApplePay];
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+    m_pPalmAnalysisView = [[DPPalmAnalysisView alloc]initWithFrame:CGRectMake(0, 0, self.view.width, self.view.height)];
+    m_pPalmAnalysisView.proDelegate = self;
+    [self.view addSubview:m_pPalmAnalysisView];
+}
+- (void)PushToNextPage:(id)argData
+{
+    NSString *product = @"com.dailypsychic.horoscope01";
+    _currentProId = product;
+    if([SKPaymentQueue canMakePayments]){
+        [self requestProductData:product];
+    }else{
+        NSLog(@"不允许程序内付费");
+    }
 }
 #pragma mark - Apple Pay
-- (void)settingApplePay
-{
-    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
-    
-    self.profuctIdArr = @[@"123"];
-    
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    
-    button.frame = CGRectMake(100, 100, 100, 100);
-    
-    button.backgroundColor = [UIColor greenColor];
-    
-    [button setTitle:@"6元" forState:UIControlStateNormal];
-    
-    [button addTarget:self action:@selector(btnClick:) forControlEvents:UIControlEventTouchDown];
-    
-    button.tag = 100;
-    
-    [self.view addSubview:button];
-}
 - (void)btnClick:(UIButton *)button
-
 {
-    
     NSString *product = self.profuctIdArr[button.tag-100];
-    
     _currentProId = product;
-    
     if([SKPaymentQueue canMakePayments]){
-        
         [self requestProductData:product];
-        
     }else{
-        
         NSLog(@"不允许程序内付费");
-        
     }
-    
 }
 //请求商品
-
 - (void)requestProductData:(NSString *)type{
-    
     NSLog(@"-------------请求对应的产品信息----------------");
-    
-    
-    
     NSArray *product = [[NSArray alloc] initWithObjects:type,nil];
-    
-    
-    
     NSSet *nsset = [NSSet setWithArray:product];
-    
     SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:nsset];
-    
     request.delegate = self;
-    
     [request start];
-    
-    
-    
 }
 //收到产品返回信息
-
 - (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response{
-    
-    
-    
     NSLog(@"--------------收到产品反馈消息---------------------");
-    
     NSArray *product = response.products;
-    
     if([product count] == 0){
-        
-        
         NSLog(@"--------------没有商品------------------");
-        
         return;
-        
     }
-    
-    
-    
     NSLog(@"productID:%@", response.invalidProductIdentifiers);
-    
     NSLog(@"产品付费数量:%lu",(unsigned long)[product count]);
-    
-    
-    
     SKProduct *p = nil;
-    
     for (SKProduct *pro in product) {
-        
         NSLog(@"%@", [pro description]);
-        
         NSLog(@"%@", [pro localizedTitle]);
-        
         NSLog(@"%@", [pro localizedDescription]);
-        
         NSLog(@"%@", [pro price]);
-        
         NSLog(@"%@", [pro productIdentifier]);
         if([pro.productIdentifier isEqualToString:_currentProId]){
-            
             p = pro;
-            
         }
-        
     }
-    
-    
-    
     SKPayment *payment = [SKPayment paymentWithProduct:p];
-    
-    
-    
     NSLog(@"发送购买请求");
-    
     [[SKPaymentQueue defaultQueue] addPayment:payment];
-    
 }
 //请求失败
-
 - (void)request:(SKRequest *)request didFailWithError:(NSError *)error{
-    
-    
     NSLog(@"------------------错误支付失败-----------------:%@", error);
-    
 }
 
-
-
 - (void)requestDidFinish:(SKRequest *)request{
-    
-    
     NSLog(@"------------反馈信息结束-----------------");
 }
 //沙盒测试环境验证
-
 #define SANDBOX @"https://sandbox.itunes.apple.com/verifyReceipt"
-
 //正式环境验证
-
 #define AppStore @"https://buy.itunes.apple.com/verifyReceipt"
 
 /**
@@ -184,193 +104,85 @@
  
  */
 
--(void)verifyPurchaseWithPaymentTransaction{
-    
+-(void)verifyPurchaseWithPaymentTransaction
+{
     //从沙盒中获取交易凭证并且拼接成请求体数据
-    
     NSURL *receiptUrl=[[NSBundle mainBundle] appStoreReceiptURL];
-    
     NSData *receiptData=[NSData dataWithContentsOfURL:receiptUrl];
-    
-    
-    
     NSString *receiptString=[receiptData base64EncodedStringWithOptions:NSDataBase64EncodingEndLineWithLineFeed];//转化为base64字符串
-    
-    
-    
     NSString *bodyString = [NSString stringWithFormat:@"{\"receipt-data\" : \"%@\"}", receiptString];//拼接请求数据
-    
     NSData *bodyData = [bodyString dataUsingEncoding:NSUTF8StringEncoding];
-    
-    
-    
-    
-    
     //创建请求到苹果官方进行购买验证
-    
     NSURL *url=[NSURL URLWithString:SANDBOX];
-    
     NSMutableURLRequest *requestM=[NSMutableURLRequest requestWithURL:url];
-    
     requestM.HTTPBody=bodyData;
-    
     requestM.HTTPMethod=@"POST";
-    
     //创建连接并发送同步请求
-    
     NSError *error=nil;
-    
     NSData *responseData=[NSURLConnection sendSynchronousRequest:requestM returningResponse:nil error:&error];
-    
     if (error) {
-        
         NSLog(@"验证购买过程中发生错误，错误信息：%@",error.localizedDescription);
-        
         return;
-        
     }
-    
     NSDictionary *dic=[NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingAllowFragments error:nil];
-    
     NSLog(@"%@",dic);
-    
     if([dic[@"status"] intValue]==0){
-        
         NSLog(@"购买成功！");
-        
         NSDictionary *dicReceipt= dic[@"receipt"];
-        
         NSDictionary *dicInApp=[dicReceipt[@"in_app"] firstObject];
-        
         NSString *productIdentifier= dicInApp[@"product_id"];//读取产品标识
-        
         //如果是消耗品则记录购买数量，非消耗品则记录是否购买过
-        
         NSUserDefaults *defaults=[NSUserDefaults standardUserDefaults];
-        
-        if ([productIdentifier isEqualToString:@"123"]) {
-            
-            int purchasedCount=[defaults integerForKey:productIdentifier];//已购买数量
-            
+        if ([productIdentifier isEqualToString:@"com.dailypsychic.horoscope01"]) {
+            long purchasedCount=[defaults integerForKey:productIdentifier];//已购买数量
             [[NSUserDefaults standardUserDefaults] setInteger:(purchasedCount+1) forKey:productIdentifier];
-            
         }else{
-            
             [defaults setBool:YES forKey:productIdentifier];
-            
         }
-        
         //在此处对购买记录进行存储，可以存储到开发商的服务器端
-        
     }else{
-        
         NSLog(@"购买失败，未通过验证！");
-        
     }
-    
 }
 
 //监听购买结果
-
 - (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transaction{
-    
-    
-    
-    
-    
     for(SKPaymentTransaction *tran in transaction){
-        
-        
-        
-        
-        
-        
-        
         switch (tran.transactionState) {
-                
-            case SKPaymentTransactionStatePurchased:{
-                
+            case SKPaymentTransactionStatePurchased:
+            {
                 NSLog(@"交易完成");
-                
                 [self verifyPurchaseWithPaymentTransaction];
-                
                 [[SKPaymentQueue defaultQueue] finishTransaction:tran];
-                
-                
-                
             }
-                
-                
-                
-                
-                
-                
-                
-                
-                
                 break;
-                
             case SKPaymentTransactionStatePurchasing:
-                
                 NSLog(@"商品添加进列表");
-                
-                
-                
                 break;
-                
-            case SKPaymentTransactionStateRestored:{
-                
+            case SKPaymentTransactionStateRestored:
+            {
                 NSLog(@"已经购买过商品");
-                
-                
-                
                 [[SKPaymentQueue defaultQueue] finishTransaction:tran];
-                
             }
-                
                 break;
-                
-            case SKPaymentTransactionStateFailed:{
-                
+            case SKPaymentTransactionStateFailed:
+            {
                 NSLog(@"交易失败");
-                
                 [[SKPaymentQueue defaultQueue] finishTransaction:tran];
-                
-                
             }
-                
                 break;
-                
             default:
-                
                 break;
-                
         }
-        
     }
-    
 }
-
-
-
 //交易结束
-
 - (void)completeTransaction:(SKPaymentTransaction *)transaction{
-    
     NSLog(@"交易结束");
-    
-    
-    
     [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
-    
 }
-
-
-
-
 
 - (void)dealloc{
-    
     [[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
     
 }
@@ -379,15 +191,15 @@
 {
     [self.navigationController popToRootViewControllerAnimated:YES];
 }
-- (void)PushToNextPage:(id)argData{
-    
-    DPPalmResultController *resultVc = [[DPPalmResultController alloc]init];
-    if ([self.analysisType isEqualToString:@"test"]) {
-        resultVc.dpResultType = DPResultTest;
-    }else if ([self.analysisType isEqualToString:@"palm"]) {
-        resultVc.dpResultType = DPResultPalm;
-    }
-    [self PushChildViewController:resultVc animated:YES];
-}
+//- (void)PushToNextPage:(id)argData{
+//    
+//    DPPalmResultController *resultVc = [[DPPalmResultController alloc]init];
+//    if ([self.analysisType isEqualToString:@"test"]) {
+//        resultVc.dpResultType = DPResultTest;
+//    }else if ([self.analysisType isEqualToString:@"palm"]) {
+//        resultVc.dpResultType = DPResultPalm;
+//    }
+//    [self PushChildViewController:resultVc animated:YES];
+//}
 
 @end
